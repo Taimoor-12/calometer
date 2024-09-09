5e2a409430bbbbf2a62e3a6ae174c4326b94baff
Taimoor Ali <taimoorali202@gmail.com>
2024-09-09 23:14:31 +0500

    add new ep and refactor prev eps

--- internal/api/handler.go
+++ internal/api/handler.go
@@ -10,6 +10,7 @@ import (
 	"calometer/internal/logger"
 
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgconn"
 	"go.uber.org/zap"
 )
 
@@ -51,6 +52,7 @@ func SignUpHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if *doesExist {
+		log.Info("username already exists")
 		http.Error(w, "Username already exists", http.StatusConflict)
 		return
 	}
@@ -73,6 +75,15 @@ func SignUpHandler(w http.ResponseWriter, r *http.Request) {
 
 	var userId uuid.UUID
 	if err := db.GetPool().QueryRow(context.Background(), qStr, user.Name, user.Username, password).Scan(&userId); err != nil {
+		if pgErr, ok := err.(*pgconn.PgError); ok && pgErr.Code == "23505" {
+			log.Info(
+				"username already exists",
+				zap.String("username", user.Username),
+			)
+			http.Error(w, "Username already exists", http.StatusConflict)
+			return
+		}
+
 		log.Info(
 			"failed to create the user",
 			zap.String("username", user.Username),
@@ -125,6 +136,14 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	userId, err := lib.GetUserIdByUsername(req.Username)
+	if err != nil {
+		log.Fatal(
+			"failed to get user id by username",
+			zap.String("username", req.Username),
+		)
+	}
+
 	exists, err := lib.DoesUserExists(req.Username)
 	if err != nil {
 		log.Info(
@@ -159,10 +178,11 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	token, err := lib.GenerateJWT(req.Username)
+	token, err := lib.GenerateJWT(*userId, req.Username)
 	if err != nil {
 		log.Info(
-			"failed to generate JWT",
+			"failed to generate JWT for user id",
+			zap.String("userId", userId.String()),
 			zap.String("username", req.Username),
 		)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
@@ -173,6 +193,73 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	resp := Response{
 		Code: http.StatusOK,
 		Data: map[string]string{"token": token},
+	}
+	json.NewEncoder(w).Encode(&resp)
+}
+
+type AddBodyDetailsReq struct {
+	Age       int     `json:"age"`
+	Weight_kg float64 `json:"weight"`
+	Height_cm int     `json:"height"`
+	Gender    string  `json:"gender"`
+}
+
+func AddBodyDetailsHandler(w http.ResponseWriter, r *http.Request) {
+	// Check for JWT in the request
+	tokenStr := lib.ExtractTokenFromHeader(r)
+	if tokenStr == "" {
+		// No token found in the request header
+		http.Error(w, "User not authenticated. Please provide a valid token.", http.StatusUnauthorized)
+		return
+	}
+
+	// Validate the JWT
+	if err := lib.ValidateToken(tokenStr); err != nil {
+		// Token is invalid
+		http.Error(w, "Invalid token. Please authenticate again.", http.StatusUnauthorized)
+		return
+	}
+
+	var req AddBodyDetailsReq
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		log.Info(
+			"failed to decode incoming json",
+			zap.Error(err),
+		)
+
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	userId, err := lib.ExtractUserIdFromToken(tokenStr)
+	if err != nil {
+		log.Info(
+			"failed to extract username from token",
+			zap.String("tokenStr", tokenStr),
+			zap.Error(err),
+		)
+	}
+
+	if err := lib.AddUserBodyDetails(
+		*userId,
+		req.Age,
+		req.Height_cm,
+		req.Weight_kg,
+		req.Gender,
+	); err != nil {
+		log.Info(
+			"failed to add user body details by id",
+			zap.String("userId", userId.String()),
+			zap.Error(err),
+		)
+
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	resp := Response{
+		Code: http.StatusOK,
 	}
 	json.NewEncoder(w).Encode(&resp)
 }
