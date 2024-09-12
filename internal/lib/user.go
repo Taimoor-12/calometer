7d5570f7f6d39aea143d108cde72f72278d3b5c2
Taimoor Ali <taimoorali202@gmail.com>
2024-09-12 20:27:57 +0500

    add bmr value into the db table

--- internal/lib/user.go
+++ internal/lib/user.go
@@ -49,27 +49,49 @@ func GetUserIdByUsername(username string) (*uuid.UUID, error) {
 }
 
 func AddUserBodyDetails(userId uuid.UUID, age int, height_cm int, weight_kg float64, gender string) error {
+	bmr := CalculateBMR(gender, age, weight_kg, height_cm)
+
 	qStr := `
 		INSERT INTO user_body_details (
 			u_id,
 			age,
 			height_cm,
 			weight_kg,
-			gender
+			gender,
+			bmr
 		) VALUES (
 			$1,
 			$2,
 			$3,
 			$4,
-			$5
+			$5,
+			$6
 		) ON CONFLICT (u_id) DO UPDATE
 		SET
 			age = COALESCE(NULLIF($2, 0), user_body_details.age),
 			height_cm = COALESCE(NULLIF($3, 0), user_body_details.height_cm),
 			weight_kg = COALESCE(NULLIF($4, 0), user_body_details.weight_kg),
-			gender = COALESCE(NULLIF($5, ''), user_body_details.gender)`
+			gender = COALESCE(NULLIF($5, ''), user_body_details.gender),
+			bmr = CASE
+				WHEN user_body_details.age <> COALESCE(NULLIF($2, 0), user_body_details.age)
+					OR user_body_details.height_cm <> COALESCE(NULLIF($3, 0), user_body_details.height_cm)
+					OR user_body_details.weight_kg <> COALESCE(NULLIF($4, 0), user_body_details.weight_kg)
+					OR user_body_details.gender <> COALESCE(NULLIF($5, ''), user_body_details.gender)
+				THEN $6
+				ELSE user_body_details.bmr
+			END
+	`
 
-	if _, err := db.GetPool().Exec(context.Background(), qStr, userId, age, height_cm, weight_kg, gender); err != nil {
+	if _, err := db.GetPool().Exec(
+		context.Background(),
+		qStr,
+		userId,
+		age,
+		height_cm,
+		weight_kg,
+		gender,
+		bmr,
+	); err != nil {
 		return err
 	}
 
