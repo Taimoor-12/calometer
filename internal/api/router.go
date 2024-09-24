a42075876154bb80b6235dfbab0ec1dbe9a0554f
Taimoor Ali <taimoorali202@gmail.com>
2024-09-24 21:06:05 +0500

    rm prev eps to update calories

--- internal/api/router.go
+++ internal/api/router.go
@@ -23,10 +23,7 @@ func SetupRouter() *mux.Router {
 	router.Handle("/api/users/log/create", authMiddleware.Then(http.HandlerFunc(CreateCalorieLogHandler))).Methods(http.MethodPost)
 	router.Handle("/api/users/log/get", authMiddleware.Then(http.HandlerFunc(GetCalorieLogs))).Methods(http.MethodGet)
 	router.Handle("/api/users/log/update", authMiddleware.Then(http.HandlerFunc(UpdateCalorieLogHandler))).Methods(http.MethodPut)
-
-	router.Handle("/api/users/log_calories/consumed", authMiddleware.Then(http.HandlerFunc(LogCaloriesConsumedHandler))).Methods(http.MethodPost)
-	router.Handle("/api/users/log_calories/burnt", authMiddleware.Then(http.HandlerFunc(LogCaloriesBurntHandler))).Methods(http.MethodPost)
-	router.Handle("/api/users/log_calories/mark_status", authMiddleware.Then(http.HandlerFunc(MarkLoggingStatusHandler))).Methods(http.MethodPost)
+	router.Handle("/api/users/log/mark_status", authMiddleware.Then(http.HandlerFunc(MarkLoggingStatusHandler))).Methods(http.MethodPost)
 
 	return router
 }
