907a24c97e4b188fec3fe68b11e65f2a1b214a66
Taimoor Ali <taimoorali202@gmail.com>
2024-11-07 13:19:55 +0500

    dashboard design

--- fe/src/Dashboard.tsx
+++ fe/src/Dashboard.tsx
@@ -1,10 +1,31 @@
 import Navigation from "./components/Navigation";
+import s from "./Dashboard.module.css";
 
 const Dashboard = () => {
   return (
-    <>
+    <div className={s.main}>
       <Navigation />
-    </>
+      <div className={s.netBalanceDiv}>
+        <p>NET CALORIC BALANCE: <span>+</span>5000</p>
+      </div>
+      <div className={s.tilesWrapperDiv}>
+        <div className={s.tilesDiv}>
+          <p>September, 2024</p>
+        </div>
+        <div className={s.tilesDiv}>
+          <p>October, 2024</p>
+        </div>
+        <div className={s.tilesDiv}>
+          <p>November, 2024</p>
+        </div>
+        <div className={s.tilesDiv}>
+          <p>November, 2024</p>
+        </div>
+      </div>
+      <div className={s.logAddDiv}>
+        <button>+</button>
+      </div>
+    </div>
   );
 }
 
