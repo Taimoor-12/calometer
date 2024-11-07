81c2213f3ba8a15d94bade98aca6cecb02e9b72f
Taimoor Ali <taimoorali202@gmail.com>
2024-11-07 18:29:48 +0500

    fetch net caloric balance from the be

--- fe/src/Dashboard.tsx
+++ fe/src/Dashboard.tsx
@@ -1,6 +1,6 @@
-import { useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import { http_post } from "./lib/http";
+import { http_get, http_post } from "./lib/http";
 import { toast } from "react-toastify";
 import Navigation from "./components/Navigation";
 import s from "./Dashboard.module.css";
@@ -9,6 +9,26 @@ const Dashboard = () => {
   const apiUrl = process.env.REACT_APP_API_URL;
   const navigate = useNavigate();
   const location = useLocation();
+
+  const [netCaloricBalance, setNetCaloricBalance] = useState(0);
+
+  const netCaloricBalanceCall = useCallback(async () => {
+    try {
+      const resp = await http_get(
+        `${apiUrl}/api/users/net_caloric_balance/get`
+      );
+      const respCode = +Object.keys(resp.code)[0];
+      if (respCode === 200) {
+        setNetCaloricBalance(resp.data.net_caloric_balance);
+      }
+    } catch (e) {
+      toast.error("Something went wrong, please try again.");
+    }
+  }, [apiUrl]);
+
+  useEffect(() => {
+    netCaloricBalanceCall();
+  }, [netCaloricBalanceCall]);
 
   useEffect(() => {
     if (location.state?.from === "login") {
@@ -38,7 +58,19 @@ const Dashboard = () => {
       <Navigation />
       <div className={s.netBalanceDiv}>
         <p>
-          NET CALORIC BALANCE: <span>+</span>5000
+          NET CALORIC BALANCE:{" "}
+          <span
+            className={
+              netCaloricBalance > 0
+                ? s.plus
+                : netCaloricBalance < 0
+                ? s.minus
+                : ""
+            }
+          >
+            {netCaloricBalance > 0 ? "+" : netCaloricBalance < 0 ? "-" : ""}
+          </span>
+          {netCaloricBalance}
         </p>
       </div>
       <div className={s.tilesWrapperDiv}>
